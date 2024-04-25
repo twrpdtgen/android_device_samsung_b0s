246ff1e66835b9ccfc629d69869ccb090b8c0eda
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2024-04-25 20:25:04 +0200

    b0s: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: samsung Model: SM-S908B Script version: 3.0.0

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_b0s.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_b0s-user \
+    omni_b0s-userdebug \
+    omni_b0s-eng
